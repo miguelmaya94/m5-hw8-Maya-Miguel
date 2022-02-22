653732788df6119c7fc1a26609364f996f17b0e4
miguelmaya94 <84256870+miguelmaya94@users.noreply.github.com>
2022-02-22 02:17:05 -0800

    edits

--- index.js
+++ index.js
@@ -15,46 +15,30 @@ var loss = document.getElementById('losses')
 
 
 // target all .box elements and attach a click event listener to each one using a loop
-var allbox = document.querySelectorAll('box')
+var allbox = document.querySelectorAll('.box')
 
-allbox.forEach(function(box) {
+allbox.forEach(function(.box) {
 
-    box.onclick = function (e) {
-        box.textContent = e.target.textContent
+    .box.onclick = function(e) {
+     
+
+        var randomwin = Math.floor(Math.random() * 3) + 1
+        console.log(randomwin)
+
+        if (allbox === randomwin) {
+            document.getElementById('message') = "You are a Winner";
+            console.log(target.e)
+            document.getElementById("wins").innerHTML;
+           
+            wins++
+        }
+        else {
+
+            parseInt.lossingmessg = document.getElementById('message')
+            document.getElementById("losses").innerHTML;
+            losses++
+        }
     }
 })
 
 
-
-// create a random number between 1-3 and store it to a variable
-// This number will represent the winning box
-var randomwin = Math.floor(Math.random() * 3) + 1 
-
-
-console.log(randomwin)
-
-// determine if the box clicked is equal to the random number
-// if the numbers match, display a winning message by changing the text content of the div#message element
-// if the numbers match, increment wins and display the win count in div#wins
-if (allbox === randomwin) {
-    document.getElementById('message') = "You are a Winner";
-
-    document.getElementById("wins").innerHTML;
-  
-    wins++
-}
-
-
-else {
- 
-    parseInt.lossingmessg = document.getElementById('message')
-    document.getElementById("losses").innerHTML;
-    losses++
-    }
-
-    
-
-
-
-// if the numbers don't match, change the div#message element's text to a random losing message from the array above
-// if the numbers don't match, increment losses and display the loss count in div#losses
