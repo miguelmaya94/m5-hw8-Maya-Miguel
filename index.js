988ea0189d757a9475eddac8f6fc6d804dfa6528
miguelmaya94 <84256870+miguelmaya94@users.noreply.github.com>
2022-02-23 01:33:27 -0800

    edits so far

--- index.js
+++ index.js
@@ -14,23 +14,34 @@ var allbox = document.querySelectorAll('.box')
   allbox.forEach(function(box) {
 
 box.onclick = function(e) {
+    //e.target.textContent
+    var newbox = e.target.textContent
+    
+    console.log(newbox)
+   
      
 
-        var randomwin = Math.floor(Math.random() * 3) + 1
-        console.log(randomwin)
 
-    if (allbox === randomwin) {
-            document.getElementById('message') = "You are a Winner";
+
+        var randomwin = Math.floor(Math.random() * 3) + 1
+    console.log(randomwin)
+
+    if (newbox === randomwin) {
+     
+
+        document.getElementById("message").innerHTML = "You are a Winner";
+        document.getElementById("message").textContent = "I have changed!";
           
-            document.getElementById("wins").innerHTML;
-           
-           wins++
+        document.getElementById("wins").innerHTML;
+        wins++
+       
         }
         else {
 
-            parseInt.lossingmessg = document.getElementById('message')
+        parseInt.lossingmessg = document.getElementById('message');
             document.getElementById("losses").innerHTML;
-            losses++
+        losses++
+        
         }
     }
 })
